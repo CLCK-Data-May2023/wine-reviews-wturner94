f0ba7ea37c058fc04601259fa3084605de1ea7c9
wturner94 <williamj.turner94@gmail.com>
2023-09-10 12:38:30 -0400

    initial commit

--- reviews.py
+++ reviews.py
@@ -1,2 +1,11 @@
-# add your code here
+import pandas as pd
 
+wineData = pd.read_csv('data/winemag-data-130k-v2.csv.zip', index_col=0)
+
+reviewsPerCountry = wineData.country.value_counts()
+
+reviewPoints = wineData.groupby('country')['points'].mean().round(1)
+
+mergedReviews = pd.DataFrame.merge(reviewsPerCountry, reviewPoints, on='country')
+
+mergedReviews.to_csv('data/reviews-per-country.csv')
